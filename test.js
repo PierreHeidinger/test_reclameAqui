974e4da9a9e3fad80f442303035d4a0a2a1cd95b
Pierre Boronda Heidinger <macpierre@MacBook-Pro-de-Pierre.local>
2019-02-09 10:02:34 -0200

    test

--- test.js
+++ test.js
@@ -20,7 +20,9 @@ test('post', (assert) => {
     var registerTest = {
         "title" : "title teste",
         "description": "google not working",
-        "company" : "google"
+        "company" : "google",
+        "locale" : {"ip":"187.10.129.216","country":"Brazil","country_code":"BR","city":"São Paulo","continent":"South America","latitude":-23.5733,"longitude":-46.6417,"time_zone":"America/Sao_Paulo","postal_code":"01323","org":"TELEFÔNICA BRASIL S.A","asn":"AS27699","subdivision":"Sao Paulo","subdivision2":null}
+
     };
 
     request(app)
@@ -29,7 +31,6 @@ test('post', (assert) => {
         .expect(200)
         .expect('Content-Type', /json/)
         .end(function (err, res) {
-      
             assert.end();
         })
 })
